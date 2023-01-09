1b0de814ba0750e5064fe16b0ae4938f37664ca1
tsaig <tsaig@LAPTOP-B95MC7DP>
2023-01-09 16:16:13 +0530

    first msg

---
+++ jenkins/src/test/java/jenkins/Testcase1.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+
+public class Testcase1 {
+@Test
+public void m1()
+
+{
+	Reporter.log("Hello",true);
+
+	
+}
+
+}
---
+++ jenkins/src/test/java/jenkins/Testcase2.java
@@ -0,0 +1,19 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+
+public class Testcase2 {
+	@Test
+	public void m2()
+	
+
+	{
+		Reporter.log("Hi",true);
+
+		
+
+	}
+
+
+}
